c43f6f7e6e43e3bb57965b4b8af3a0d9774e2aab
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2019-08-01 10:01:24 -0500

    descuentoAndDescripcion

--- app/Models/Costo_turno.php
+++ app/Models/Costo_turno.php
@@ -1,5 +1,11 @@
 <?php
 
-namespace 
+namespace App\models;
+use Illuminate\Database\Model;
 
-?>
+class Costo_turno extends Model {
+    protected $table = 'costo_turnos';
+    protected $primaryKey = 'id_costo';
+    const CREATED_AT  = 'create_at';
+    const UPDATED_AT = 'update_at';
+}
