150f4b1378996bb825146d1040e146bdafc68905
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2019-08-01 07:52:59 -0500

    models/controler

--- public/index.php
+++ public/index.php
@@ -1,2 +1,48 @@
 <?php
 
+
+ini_set('display_errors', 1); //inicializa errores
+ini_set('display_starup_error', 1); //inicializa errores
+error_reporting(E_ALL);//mostrar errores en pantalla
+
+require_once '../vendor/autoload.php';
+
+
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'pgsql',
+    'host'      => 'localhost',
+    'port'      =>  '5432',
+    'database'  => 'recaudo_prueba',
+    'username'  => 'postgres',
+    'password'  =>  '1113645020',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+    ]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
