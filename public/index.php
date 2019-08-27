3358af68a0eedf04e2da8874ed078991251cc4b4
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2019-08-27 11:22:00 -0500

    dataBase

--- public/index.php
+++ public/index.php
@@ -32,6 +32,7 @@ $capsule->addConnection([
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
     ]);
+
 // Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
 
